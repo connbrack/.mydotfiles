66e58be021ea4f73b700ca63789860d17ec8019e
Connor B <80982854+connbrack@users.noreply.github.com>
2024-02-26 11:40:39 -0500

    print link created only if successful

--- config.sh
+++ config.sh
@@ -7,23 +7,18 @@ link_file_to_home() {
     local source_file=$1$2
     local target_link="$HOME/$2"
 
-    # Check if the file already exists in the home directory
+    # Check if the file already exists in the home directory if not, create symbolic link
     if [ -L "$target_link" ]; then
-        # Check if the existing link points to the same file
         if [ "$(readlink -f "$target_link")" == "$source_file" ]; then
             :
         else
             echo "A link for '$file' already exists but points to a different file. Please remove link and re-run script."
         fi
     elif [ -e "$target_link" ]; then
-        # Backup the existing file
         echo "A file for '$file' already exists. Please remove or rename file and re-run script.."
     else
-        # Create a symbolic link in the home directory
-        ln -s "$source_file" "$target_link"
-        echo "Link created: $target_link -> $source_file"
+        ln -s "$source_file" "$target_link" && echo "Link created: $target_link -> $source_file"
     fi
-
 }
 
 dotfilelocations="$HOME/.mydotfiles/dotfiles/"
