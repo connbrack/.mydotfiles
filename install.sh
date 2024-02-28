d8eae081514afb904038d9f67777c0e5deb54560
connbrack <connor.brackley@mail.concordia.ca>
2024-02-28 00:37:56 -0500

    Updated install script

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ sudo apt upgrade -y &&
 
 # ----------------------------- Install tools -------------------------------------------
 
-sudo apt install gtk3-nocsd
 sudo apt install fzf -y
 sudo apt install trash-cli -y
 
@@ -36,16 +35,14 @@ make -C ble.sh install PREFIX=~/.local
 #export NVM_DIR="$HOME/.nvm"
 #source ~/.bashrc
 #nvm install --lts
-#npm install tldr
+#npm install -g tldr 
 
-## Install miniconda - https://www.youtube.com/watch?v=i7q8JbNK-9s
 #mkdir -p ~/miniconda3
 #wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 #bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 #rm -rf ~/miniconda3/miniconda.sh
 #~/miniconda3/bin/conda init bash
-#
-#
+
 ## Create swap storage - https://www.youtube.com/watch?v=i7q8JbNK-9s
 #sudo dd if=/dev/zero of=/mnt/swapfile bs=1024 count=2097152
 #sudo fallocate --length 2GiB /mnt/swapfile
