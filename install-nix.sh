75833020556fd964d51ebd6a01ff57c1517eabb0
Connor B <80982854+connbrack@users.noreply.github.com>
2024-02-28 23:48:30 -0500

    added nvm stuff

--- install-nix.sh
+++ install-nix.sh
@@ -29,7 +29,8 @@ nix-env -iA nixpkgs.tldr
 ## npm packages 
 #curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 #export NVM_DIR="$HOME/.nvm"
-#source ~/.bashrc
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 #nvm install --lts
 
 #mkdir -p ~/miniconda3
