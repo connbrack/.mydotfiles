92b8c91636a23e9458b5db88e87c9de1ec52fe00
Connor B <80982854+connbrack@users.noreply.github.com>
2024-03-03 22:33:47 -0500

    cleaned up and added force function

--- config.sh
+++ config.sh
@@ -1,25 +1,30 @@
 #bin/bash
 
+# Default behavior
+force=0; 
+
+# Process flags
+for arg in "$@"; do
+  case $arg in
+      -f) force=1 ;;
+      *) echo "Unknown option: $arg" exit 1 ;;
+  esac
+done
+
 # ----------------------------- Link Configuration Files -------------------------------------------
 
 link_file_to_home() {
-    local file=$2
     local source_file=$1$2
     local target_link="$HOME/$2"
 
-    # Check if the file already exists in the home directory if not, create symbolic link
-    if [ -L "$target_link" ]; then
-        if [ "$(readlink -f "$target_link")" == "$source_file" ]; then
-            :
-        else
-            echo "A link for '$file' already exists but points to a different file. Please remove link and re-run script."
-        fi
-    elif [ -e "$target_link" ]; then
-        echo "A file for '$file' already exists. Please remove or rename file and re-run script.."
+    if [ force -eq 1 ]; then
+      ln -sf "$source_file" "$target_link" && echo "Link created: $target_link -> $source_file"
     else
-        ln -s "$source_file" "$target_link" && echo "Link created: $target_link -> $source_file"
+      ln -s "$source_file" "$target_link" && echo "Link created: $target_link -> $source_file"
     fi
 }
+
+# --------------------------------- Options -------------------------------------------------
 
 dotfilelocations="$HOME/.mydotfiles/dotfiles/"
 files_to_link=(
@@ -27,15 +32,24 @@ files_to_link=(
 	".bashrc_functions" 
 	".bashrc_alias" 
 	".blerc" 
+	".config/tmux" 
 	".config/nvim" 
 	".config/lf" 
 )
 
+# --------------------------------- Run script -------------------------------------------------
+
+# Make .config file if it does not exist
 if ! [ -d "$HOME/.config" ]; then
   mkdir $HOME/.config
 fi
 
-
+# Main script 
 for file in "${files_to_link[@]}"; do
     link_file_to_home "$dotfilelocations" "$file"
 done
+
+# run conda init if conda is not installed
+if command -v node >/dev/null 2>&1; then
+  conda init
+fi
