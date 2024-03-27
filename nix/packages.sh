3b3bcae22dd77a9e71b8683c7e67dfba9baacdef
connbrack <connor.brackley@gmail.com>
2024-03-26 23:42:22 -0400

    changed to nix profile

---
+++ nix/packages.sh
@@ -0,0 +1,26 @@
+
+# Main terminal
+neovim
+lf
+tmux
+fzf
+btop
+starship
+
+# Extras
+tldr
+tgpt
+pokeget-rs
+cmatrix
+cowsay
+
+# Progamming
+nodejs_21
+yarn
+
+# Graphical Apps
+firefox
+vscodium
+audacity
+logseq
+zotero
