9551a6b2d871ec71b234ea7128a5f4ca3cdc46fd
Connor B <80982854+connbrack@users.noreply.github.com>
2024-05-22 15:33:40 -0400

    moved nix config

---
+++ Nix/packages.sh
@@ -0,0 +1,26 @@
+
+# Main terminal
+neovim
+lf
+tmux
+fzf
+btop
+
+# Extras
+#starship
+#tldr
+#tgpt
+#pokeget-rs
+#cmatrix
+#cowsay
+
+# Progamming
+#nodejs_21
+#yarn
+
+# Graphical Apps
+#firefox
+#vscodium
+#audacity
+#logseq
+#zotero
