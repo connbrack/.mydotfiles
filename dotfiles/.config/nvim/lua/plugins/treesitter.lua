53eb1e09667c7e11135ef201ef875522790b2f3f
Connor B <noreply@git.com>
2024-05-22 15:32:43 -0400

    cant stop wont stop

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -5,9 +5,9 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = {"lua", "javascript", "python"},
+        ensure_installed = { 'vim', 'vimdoc', 'query', 'c', 'cpp', 'go', 'lua', 'python', 'javascript', 'rust', 'typescript', 'cmake' },
         highlight = { enable = true },
-        indent = { enable = true },  
+        indent = { enable = true },
       })
     end
   }
