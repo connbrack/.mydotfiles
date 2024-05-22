fd69df2f5cb1e8a201cd31cc7ce412319a927dfc
connbrack <connor.brackley@gmail.com>
2024-05-22 15:33:40 -0400

    moved nix config

---
+++ dotfiles/.config/nix/packages.sh
@@ -0,0 +1,27 @@
+
+# Main terminal
+neovim
+lf
+tmux
+fzf
+btop
+#starship
+
+# Extras
+#texliveFull
+#lua
+#tldr
+#tgpt
+#pokeget-rs
+#cmatrix
+
+# Graphical Apps
+#firefox
+#vscodium
+#audacity
+#logseq
+#zotero
+
+# Phone stuff
+#android-tools
+#scrcpy
