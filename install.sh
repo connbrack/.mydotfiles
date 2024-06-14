7e6a2ec26ddb8fecd49258f614cc04a495630df4
connbrack <connor.brackley@gmail.com>
2024-06-13 23:06:32 -0400

    Moving things around

--- install.sh
+++ install.sh
@@ -109,7 +109,7 @@ if [ $nixpac -eq 1 ]; then
   else
     curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
     . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-    cp -r ~/.mydotfiles/Nix/ ~/
+    cp -r ~/.mydotfiles/others/Nix/ ~/
     ~/Nix/my-home-manager.sh
   fi
 fi
