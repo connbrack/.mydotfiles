008c432c43b955c39553e4ee4c416bdbb5eadc1a
Connor Brackley <noreply@github>
2024-06-13 22:14:08 -0400

    Vim keeps getting better

--- dotfiles/.config/nvim/lua/plugins/vimux.lua
+++ dotfiles/.config/nvim/lua/plugins/vimux.lua
@@ -2,7 +2,7 @@ return {
   {
     'preservim/vimux',
     config = function()
-      vim.keymap.set('n', '<leader>rl', ':w | VimuxRunLastCommand<CR>', {})
+      vim.keymap.set('n', '<leader>rr', ':w | VimuxRunLastCommand<CR>', {})
       vim.keymap.set('n', '<leader>rk', ':VimuxInterruptRunner<CR>', {})
       vim.keymap.set('n', '<leader>rc', ':VimuxPromptCommand<CR>', {})
     end
