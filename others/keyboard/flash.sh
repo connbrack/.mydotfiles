092a7fd864740246a6441124a1b003395819a7da
connbrack <connor.brackley@gmail.com>
2024-06-18 01:11:34 -0400

    keeboard keeps getting better

--- others/keyboard/flash.sh
+++ others/keyboard/flash.sh
@@ -1,8 +1,9 @@
 
 sudo true
+echo 'sleeping for 3 seconds'
 
 sleep 3
 
 sudo dfu-programmer atmega32u4 erase
-sudo dfu-programmer atmega32u4 flash QMKool_corne_layout.hex 
+sudo dfu-programmer atmega32u4 flash my-corne.hex
 sudo dfu-programmer atmega32u4 reset
