da3e9f6c5f0c2fd0edc64f9af9628204e5ad926a
Connor B <80982854+connbrack@users.noreply.github.com>
2024-06-25 10:28:12 -0400

    More more more

--- dotfiles/.config/nvim/lua/plugins/telescope.lua
+++ dotfiles/.config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,14 @@
 return {
   {
     'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 
+      "nvim-lua/plenary.nvim" ,
+      "debugloop/telescope-undo.nvim",
+    },
     config = function()
       local telescope = require("telescope")
       local builtin = require("telescope.builtin")
+      require("telescope").load_extension("undo")
       telescope.setup {
         defaults = {
           file_ignore_patterns = {
@@ -26,8 +30,10 @@ return {
           },
         },
       }
+
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
       vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
     end
   },
 }
