e6bfccfa193557c32a9162dc3c3055bcac95dee5
Connor B <80982854+connbrack@users.noreply.github.com>
2024-07-16 12:27:47 -0400

    what a hunk

--- dotfiles/.config/nvim/lua/plugins/gitsigns.lua
+++ dotfiles/.config/nvim/lua/plugins/gitsigns.lua
@@ -5,6 +5,9 @@ return {
       require("gitsigns").setup()
 
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gu", ":Gitsigns reset_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gl", ":Gitsigns next_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gh", ":Gitsigns previous_hunk<CR>", {})
     end
   }
 }
