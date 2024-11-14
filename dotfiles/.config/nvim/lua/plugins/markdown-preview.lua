bf89eb211c6bd61f972652b1d217fe24263e02eb
Connor B <noreply@git.com>
2024-11-14 15:33:53 -0500

    nvimmmmmmmm mostly markdown

--- dotfiles/.config/nvim/lua/plugins/markdown-preview.lua
+++ dotfiles/.config/nvim/lua/plugins/markdown-preview.lua
@@ -2,7 +2,14 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-  }
+
+    config = function()
+      vim.keymap.set("n", "<leader>mp", ":MarkdownPreview<CR>", {})
+    end
+  },
 }
