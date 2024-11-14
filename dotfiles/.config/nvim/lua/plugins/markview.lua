8b9891dd21ddf5870363fcc44b276f7e0b46a4e2
Connor Brackley <noreply@git.com>
2024-11-14 15:33:53 -0500

    nvimmmmmmmm mostly markdown

---
+++ dotfiles/.config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,18 @@
+return {
+  "OXY2DEV/markview.nvim",
+  lazy = false, -- Recommended
+  -- ft = "markdown" -- If you decide to lazy-load anyway
+
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons"
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>mt", ":Markview toggle<CR>", {})
+    vim.keymap.set("n", "<leader>ms", ":Markview splitToggle<CR>", {})
+
+    require('markview').setup({
+      initial_state = false,
+    })
+  end
+}
