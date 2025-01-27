0f9428364b210888dd2c7eeb273bd253051d11a2
Connor B <noreply@git.com>
2025-01-26 23:49:50 -0500

    user friendly keeb flash

--- misc/keyboard/flash.sh
+++ misc/keyboard/flash.sh
@@ -1,9 +1,12 @@
 
 sudo true
+
+select firmware in *.hex ; do break; done
+
 echo 'sleeping for 3 seconds'
 
 sleep 3
 
 sudo dfu-programmer atmega32u4 erase
-sudo dfu-programmer atmega32u4 flash my-corne-36.hex
+sudo dfu-programmer atmega32u4 flash $firmware
 sudo dfu-programmer atmega32u4 reset
