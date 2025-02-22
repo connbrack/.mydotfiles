34e57d5529f0b21bbcb711fa8921f02c2c1bbea9
Connor Brackley <noreply@git.com>
2025-02-21 19:23:55 -0500

    'VIMMMMMMMMMMMMMM is sick

--- dotfiles/.config/nvim/lua/plugins/debugging.lua
+++ dotfiles/.config/nvim/lua/plugins/debugging.lua
@@ -7,7 +7,7 @@ return {
   },
   config = function()
     local dap, dapui = require("dap"), require("dapui")
-    require("dapui").setup()
+    dapui.setup()
     require("dap-python").setup("~/.local/share/nvim/python-env/bin/python")
 
     dap.listeners.before.attach.dapui_config = function()
@@ -16,28 +16,32 @@ return {
     dap.listeners.before.launch.dapui_config = function()
       dapui.open()
     end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+
+    -- use Esc to close the dap-float window type
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "dap-float",
+      callback = function()
+        vim.api.nvim_buf_set_keymap(0, "n", "<Esc>", "<cmd>close!<CR>", { noremap = true, silent = true })
+      end
+    })
 
     vim.keymap.set('n', '<leader>dt', dap.toggle_breakpoint, {})
     vim.keymap.set('n', '<leader>dc', dap.continue, {})
     vim.keymap.set('n', '<leader>ds', dap.terminate, {})
     vim.keymap.set('n', '<leader>dn', dap.step_into, {})
+    vim.keymap.set('n', '<leader>dr', dap.restart, {})
+    vim.keymap.set('n', '<leader>du', dapui.toggle, {})
+    vim.keymap.set('n', '<leader>di', require('dap.ui.widgets').hover, {})
 
     -- dap.configurations.python = {
     --   {
-    --     type = 'python';
-    --     request = 'launch';
-    --     name = "Launch file";
-    --     program = "${command:pickFile}";
+    --     type = 'python',
+    --     request = 'launch',
+    --     name = "Launch file",
+    --     program = "${file}",
+    --     console = 'integratedTerminal',
+    --     cwd = '${workspaceFolder}',
     --   },
     -- }
-    --
-
-
   end
 }
