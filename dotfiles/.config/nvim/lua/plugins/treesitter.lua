6afa9b316c2851835105e6ac4829b779c359dc5c
Connor Brackley <noreply@git.com>
2025-03-07 00:44:15 -0500

    Nvim cleanup and make pretty

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = { 'vim', 'vimdoc', 'query', 'c', 'cpp', 'go', 'lua', 'python', 'javascript', 'rust', 'typescript', 'cmake' },
+        auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
       })
