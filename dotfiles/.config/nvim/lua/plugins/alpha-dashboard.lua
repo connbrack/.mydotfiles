903717417d1b593e67078ab232fa3534c4b986ff
Connor B <80982854+connbrack@users.noreply.github.com>
2025-03-07 00:44:15 -0500

    Nvim cleanup and make pretty

--- dotfiles/.config/nvim/lua/plugins/alpha-dashboard.lua
+++ dotfiles/.config/nvim/lua/plugins/alpha-dashboard.lua
@@ -1,16 +1,19 @@
 return {
   {
     "goolord/alpha-nvim",
-    -- dependencies = { 'echasnovski/mini.icons' },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
       local startify = require("alpha.themes.startify")
-      -- available: devicons, mini, default is mini
-      -- if provider not loaded and enabled is true, it will try to use another provider
       startify.file_icons.provider = "devicons"
-      require("alpha").setup(
-        startify.config
-      )
+      startify.section.header.val = {
+      [[                                       ]],
+      [[         |\      _,,,---,,_            ]],
+      [[   ZZZzz /,`.-'`'    -.  ;-;;,_        ]],
+      [[        |,4-  ) )-,_. ,\ (  `'-'       ]],
+      [[       '---''(_/--'  `-'\_)            ]],
+
+      }
+      require("alpha").setup(startify.config)
     end,
   },
 }
