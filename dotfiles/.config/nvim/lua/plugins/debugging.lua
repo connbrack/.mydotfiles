a1688787a880c01eaac4280baec8268397830ad7
Connor B <80982854+connbrack@users.noreply.github.com>
2025-06-07 23:44:17 -0400

    More NVIM what else is new

--- dotfiles/.config/nvim/lua/plugins/debugging.lua
+++ dotfiles/.config/nvim/lua/plugins/debugging.lua
@@ -8,7 +8,7 @@ return {
   config = function()
     local dap, dapui = require("dap"), require("dapui")
     dapui.setup()
-    require("dap-python").setup("~/.local/share/nvim/python-env/bin/python")
+    require("dap-python").setup("~/.local/share/nvim/mason/packages/debugpy/venv/bin/python")
 
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
@@ -33,15 +33,57 @@ return {
     vim.keymap.set('n', '<leader>du', dapui.toggle, {})
     vim.keymap.set('n', '<leader>di', require('dap.ui.widgets').hover, {})
 
-    -- dap.configurations.python = {
-    --   {
-    --     type = 'python',
-    --     request = 'launch',
-    --     name = "Launch file",
-    --     program = "${file}",
-    --     console = 'integratedTerminal',
-    --     cwd = '${workspaceFolder}',
-    --   },
-    -- }
+
+    -- *******************************************
+    -- ************** PYTHON *********************
+    -- *******************************************
+
+    dap.configurations.python = {
+      {
+        name = "File",
+        type = "python",
+        request = "launch",
+        program = "${file}",
+        cwd = vim.fn.getcwd(),
+        justMyCode = true,
+        console = "integratedTerminal",
+      },
+      {
+        name = "File:args",
+        type = "python",
+        request = "launch",
+        program = "${file}",
+        args = function()
+          local input = vim.fn.input('Arguments: ')
+          return vim.fn.split(input, " ", true)
+        end,
+        cwd = vim.fn.getcwd(),
+        justMyCode = true,
+        console = "integratedTerminal",
+      },
+      {
+        name = "Module",
+        type = "python",
+        request = "launch",
+        module = function()
+          return vim.fn.input('Module name (e.g. path.to.module): ')
+        end,
+        args = function()
+          local input = vim.fn.input('Arguments: ')
+          return vim.fn.split(input, " ", true)
+        end,
+        cwd = vim.fn.getcwd(),
+        justMyCode = true,
+        console = "integratedTerminal",
+      },
+      {
+        type = 'python',
+        request = 'launch',
+        name = "Debug pytest method",
+        module = 'pytest',
+        args = { "${file}" },
+        justMyCode = false,
+      },
+    }
   end
 }
