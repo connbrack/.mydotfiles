ab7957691176e77b11ddbed82375b39d9f72e1d6
Connor B <noreply@git.com>
2025-07-13 18:10:52 -0400

    some stuff idk

--- install.sh
+++ install.sh
@@ -97,6 +97,11 @@ if [ $packagemanager = "apt" ] && [ $termpac -eq 1 ]; then
     sudo apt install -y bat
     mkdir -p ~/.local/bin
     ln -s /usr/bin/batcat ~/.local/bin/bat
+
+    # fd
+    sudo apt install -y fd-find
+    mkdir -p ~/.local/bin
+    ln -s $(which fdfind) ~/.local/bin/fd
 fi
 
 if [ $packagemanager = "dnf" ] && [ $termpac -eq 1 ]; then
