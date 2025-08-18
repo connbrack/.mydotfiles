b663870a901a32babe5bb761c94d56e7d0760cbc
Connor B <noreply@git.com>
2025-08-17 22:56:35 -0400

    stow! and other install script improvements

--- config.sh
+++ config.sh
@@ -1,62 +1,5 @@
 #bin/bash
 
-# Default behavior
-force=0; 
-
-# Process flags
-for arg in "$@"; do
-  case $arg in
-      -f) force=1 ;;
-      *) echo "Unknown option: $arg" exit 1 ;;
-  esac
-done
-
-# ----------------------------- Link Configuration Files -------------------------------------------
-
-link_file_to_home() {
-    local source_file=$1$2
-    local target_link="$HOME/$2"
-
-    if [ "$(readlink -f "$target_link")" == "$source_file" ]; then
-      return 0
-    fi
-    
-    if [ $force -eq 1 ]; then
-      ln -sf "$source_file" "$target_link" && echo "Link created: $target_link -> $source_file"
-    else
-      ln -s "$source_file" "$target_link" && echo "Link created: $target_link -> $source_file"
-    fi
-}
-
-# --------------------------------- Options -------------------------------------------------
-
-dotfilelocations="$HOME/.mydotfiles/dotfiles/"
-files_to_link=(
-	".bashrc" 
-	".bash" 
-	".blerc" 
-  ".gitconfig"
-  ".config/bat"
-  ".config/bottom"
-  ".config/lf"
-  ".config/nvim"
-  ".config/starship.toml"
-  ".config/tmux"
-  #".config/rofi"
-)
-
-# --------------------------------- Run script -------------------------------------------------
-
-# Make .config file if it does not exist
-if ! [ -d "$HOME/.config" ]; then
-  mkdir $HOME/.config
-fi
-
-# Main script 
-for file in "${files_to_link[@]}"; do
-    link_file_to_home "$dotfilelocations" "$file"
-done
-
 if command -v tmux >/dev/null 2>&1; then
   if ! [ -d $HOME/.local/share/tmux/plugins ]; then
     mkdir -p $HOME/.local/share/tmux/plugins
@@ -66,13 +9,4 @@ fi
 
 if command -v bat >/dev/null 2>&1; then
   \bat cache --build
-fi
-
-if command -v uv >/dev/null 2>&1 && [ ! -d ~/.uv ]; then
-  mkdir ~/.uv
-  uv --directory ~/.uv venv
-fi
-
-if [ -d $HOME/miniconda3 ]; then
-    ~/miniconda3/bin/conda init bash
 fi
---
+++ dotfiles/.config/home-manager/home.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, ... }:
+
+{
+  nix = {
+    package = pkgs.nixVersions.stable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  home.username = builtins.getEnv "USER";
+  home.homeDirectory = "/home/${config.home.username}";
+  home.stateVersion = "24.05";
+
+  home.packages = with pkgs; [
+
+    ## Tui
+    neovim
+    lf
+    tmux
+    bottom
+    starship
+    lazygit
+
+    ## Core-tools
+    fzf
+    ripgrep
+    fd
+    bat
+    fx
+
+    ## Extras
+    tealdeer
+    tgpt
+    pokeget-rs
+    cmatrix
+    cowsay
+
+  ];
+
+  programs.home-manager.enable = true;
+  news.display = "silent";
+
+  # Based on https://github.com/nix-community/home-manager/issues/432
+  programs.man.enable = false;
+  home.extraOutputsToInstall = [ "man" ];
+}
