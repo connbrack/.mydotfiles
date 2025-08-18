ea5c63a827e9abacbc4c5e8c4da9fe1549fc33b6
Connor B <noreply@git.com>
2025-08-17 22:56:27 -0400

    misc

--- misc/firefox/install.sh
+++ misc/firefox/install.sh
@@ -1,5 +1,2 @@
-
-
-ln -s ~/.mydotfiles/misc/firefox/user.js ~/.mozilla/firefox/sj02lbuf.default-release/user.js
-ln -s ~/.mydotfiles/misc/firefox/chrome/ ~/.mozilla/firefox/sj02lbuf.default-release/chrome
-
+ln -s ~/.mydotfiles/misc/firefox/user.js ~/.mozilla/firefox/*.default-release/user.js
+ln -s ~/.mydotfiles/misc/firefox/chrome/ ~/.mozilla/firefox/*.default-release/chrome
